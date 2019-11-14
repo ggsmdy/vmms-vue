bc237d48378fce0c338550fe50824afa09253628
admin <z1113790851@163.com>
2019-11-14 16:09:49 +0800

    /***/

--- config/url.js
+++ config/url.js
@@ -1,6 +1,6 @@
 import env from './env'
 
 const DEV_URL = 'http://192.168.0.115:8000'//http://localhost:8000http://192.168.0.135:8000
-const PRO_URL = 'https://webser.hnrbwl.com'//vue.penghaoran.com
+const PRO_URL = 'http://localhost:8000'
 
 export default env === 'development' ? DEV_URL : PRO_URL
